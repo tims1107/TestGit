d152484e74140c2aec5c08a93771b46c0344896c
tim smithers <tim.smithers@developerinc.com>
2016-05-25 22:29:10 -0400

    initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Main {
+
+	public Main(){
+		run();
+	}
+	
+	
+	public static void main(String[] args) {
+		new Main();
+
+	}
+	
+	public void run(){
+		System.out.println("This is a test");
+	}
+
+}
